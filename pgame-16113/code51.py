2d5ab62c31e79bb773036ed40cfca4c3425d5ff0
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-15 11:58:36 +0900

    praktikum5

---
+++ pgame-16113/code51.py
@@ -0,0 +1,21 @@
+# Simpan dengan nama : code51.py 
+# Pemrograman Game Praktikum 5 
+# latihan code 5.1 : List (lanjutan) 
+
+# Buat variabel dan memberikan elemen 
+buah=["Durian","Mangga","Rambutan"]
+ 
+# print jumlah element list 
+print("Jumlah Element :", len(buah))
+ 
+# tambah element yang sama 
+buah.append("Mangga")
+ 
+# hitung element “Mangga” → hasil 2 
+print("Jumlah Buah Mangga:", buah.count("Mangga"))
+ 
+# Tambah list ke dalam list buah 
+buah.append(["Salak","Nangka","Apel"])
+ 
+# print list buah setelah ditambah 
+print("List Buah :", buah)
