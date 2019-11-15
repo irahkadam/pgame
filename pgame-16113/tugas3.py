f3887a697bfc8ad86426fba29b5736d7485fe1b4
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-15 11:54:20 +0900

    tugas3

---
+++ pgame-16113/tugas3.py
@@ -0,0 +1,12 @@
+# Ini Tugas 3. Praktik Git untuk MK Pemrograman Game
+# juga untuk latihan git push dan pull from terminal
+
+# Latihan Buat Variabel : 
+nama = "Nama 		: Siti Humairah I Kadam" 
+print (nama)
+npm = "NPM 		: 121055520116113" 
+print (npm)
+semester = "Semester 	: VII (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
