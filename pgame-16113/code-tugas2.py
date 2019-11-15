a066909aefc5b366ebdb3cb35d5ad6ec9470c91f
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-15 11:52:51 +0900

    Tugas2

---
+++ pgame-16113/code-tugas2.py
@@ -0,0 +1,8 @@
+# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print ("Hello, my friends") #Halo, teman-teman
+print ("my name is Siti Humairah I Kadam") #nama saya SITI HUMAIRAH I KADAM
+print ("How are you") #apakabar
+#baris ini tidak akan dieksekusi
+print ("computing friends...") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
