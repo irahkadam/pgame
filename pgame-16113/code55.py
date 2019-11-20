4cb18ee55d477472184fee342208d259b318c8e5
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-21 00:22:35 +0900

    praktikum5

---
+++ pgame-16113/code55.py
@@ -0,0 +1,26 @@
+# latihan code 5.5 : Kondisional 
+
+# Buat variabel A dan B 
+A = 50 
+B = 20 
+
+# menggunakan kondisionak if 
+if A > B: 
+	print("A lebih besar dari B") 
+
+# setelah tanda “:” merupakan  awal blok, 
+# penulisan harus masuk beberapa spasi 
+# atau menggunakan tab
+
+# menggunakan kondisionak if, elif 
+if A > B: 
+	print("A lebih besar dari B") 
+elif A == B: 
+	print("A sama dengan B") 
+# menggunakan kondisionak if, elif, else 
+if A > B: 
+	print("A lebih besar dari B") 
+elif A == B: 
+	print("A sama dengan B") 
+else: 
+	print("A Lebih Kecil dari B")
