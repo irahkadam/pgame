c26e9b8aaf8f69dea8f4567b6db427664442d03e
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-21 00:27:29 +0900

    Modul_pgame_praktikum_5

---
+++ pgame-16113/tugas_code6.py
@@ -0,0 +1,18 @@
+@@ -0,0 +1,17 @@
+# Tugas_code6 : Menggunakan Kondisional
+
+nilai = 100
+if nilai > 80:		# menggunakan kondisional if
+    print ("Sangat Baik")
+
+nilai = 80
+if nilai > 65 :		# menggunakan kondisional if
+    print ("Cukup Baik")
+
+nilai = 65
+if nilai > 50 :		# menggunakan kondisional if
+    print ("Baik")
+
+nilai = 30
+if nilai > 25 :		# menggunakan kondisional if
+    print ("Kurang") 
