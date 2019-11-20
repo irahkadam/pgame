db419d5654bd2cf8ac5fecbb2d71bfb525632d32
irahkadam <56121145+irahkadam@users.noreply.github.com>
2019-11-21 00:19:55 +0900

    praktikum5

---
+++ pgame-16113/code54.py
@@ -0,0 +1,17 @@
+# latihan code 5.4 : Sets 
+
+# Buat variabel set dan memberikan nilai elemen 
+buah = {'Durian', 'Mangga', 'Salak', 'Mangga','Salak',
+    'Durian'} 
+	
+# print set 
+print("Element Set :", buah) 
+
+# menambah satu elemen ke dalam set 
+buah.add("Melon") 
+
+# menambah lebih dari satu elemen ke set 
+buah.update(["Pepaya", "Semangka"])
+ 
+# print set setelah perubahan penambahan 
+print("Element Set :", buah)
